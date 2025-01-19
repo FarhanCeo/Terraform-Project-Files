adca3382579a160d0a24767314bc8df787d50bbe
Farhan Ahmed <80202023+FarhanCeo@users.noreply.github.com>
2025-01-19 13:53:10 +0530

    Add files via upload

---
+++ GithubAction-terrafrom/main.tf
@@ -0,0 +1,112 @@
+# Specify the provider
+provider "azurerm" {
+  features {}
+  subscription_id = "b6bc018a-adec-45c6-bf16-bd8a24b927ac"
+}
+
+# Define the resource group
+resource "azurerm_resource_group" "example" {
+  name     = "GithubActionTerraform_New"
+  location = "Central India"
+}
+
+# Define the virtual network
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Define the subnet
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Define the public IP address
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static" # Can be "Static" or "Dynamic"
+}
+
+# Define the network security group (NSG)
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nic-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Add a security rule to open port 22 for SSH
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name                        = "allow-ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.example.name
+  network_security_group_name = azurerm_network_security_group.example.name
+}
+
+# Define the network interface
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "example-ip-config"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+# Associate NSG with the subnet
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+# Define the virtual machine
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_DS1_v2"
+
+  admin_username = "adminuser"
+  admin_password = "P@ssw0rd123!" # Replace with a secure password
+
+  network_interface_ids = [azurerm_network_interface.example.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  disable_password_authentication = false
+}
+
+# Output the VM's private and public IP addresses
+output "vm_private_ip" {
+  value = azurerm_network_interface.example.private_ip_address
+}
+
+output "vm_public_ip" {
+  value = azurerm_public_ip.example.ip_address
+}
